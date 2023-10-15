5dc4f7c35ac70e5be2ab66d6b7cb78bc3bfa7a68
amaliacia <amaliasuciati00@gmail.com>
2023-10-16 00:04:29 +0700

    bbb

--- modul4/practice4/addjs.js
+++ modul4/practice4/addjs.js
@@ -7,3 +7,33 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "https://www.netflix.com/id/";
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ambil elemen "Lanjut?" berdasarkan ID
+    const lanjutButton = document.getElementById("lanjutHXH");
+
+    lanjutButton.addEventListener("click", function () {
+        console.log('click');   
+        window.location.href = "https://www.netflix.com/title/70300472";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ambil elemen "Lanjut?" berdasarkan ID
+    const lanjutButton = document.getElementById("lanjutAC");
+
+    lanjutButton.addEventListener("click", function () {
+        console.log('click');   
+        window.location.href = "https://www.netflix.com/title/80045948";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ambil elemen "Lanjut?" berdasarkan ID
+    const lanjutButton = document.getElementById("lanjutBSD");
+
+    lanjutButton.addEventListener("click", function () {
+        console.log('click');   
+        window.location.href = "https://www.netflix.com/title/80132126";
+    });
+});
